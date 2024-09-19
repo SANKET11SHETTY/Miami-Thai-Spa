e6d766e927fc3557e3db46e1e39ed803f734b2f6
Sanket Shetty <legendhimself007@gmail.com>
2024-09-19 21:45:56 +0530

    GSAP

--- script.js
+++ script.js
@@ -1,11 +1,42 @@
-// Dark Mode
+// Auto Type
+let typed = new Typed(".auto-typers", {
+  strings: ["Your Soul"],
+  typeSpeed: 150,
+  backSpeed: 150,
+  loop: true,
+});
 
-$(document).ready(function () {
-    $(".darkmoding").click(function () {
-      $("body").toggleClass("darkModer");
-    });
+
+// Gsap - Cursor
+var cursor = document.querySelector("#cursor");
+window.addEventListener("mousemove", function(event){
+  gsap.to(cursor, {
+    x: event.x,
+    y: event.y,
+    duration: 1,
+    ease: "back.out",
   });
+  console.log(event);
+});
 
+// AOS Animation
+AOS.init({
+  offset: 200,
+});
+
+// Dark Mode
+$(document).ready(function () {
+  $(".darkmoding").click(function () {
+    $("body").toggleClass("darkModer");
+  });
+});
+
+// Pre Loader
+var loader = document.getElementById("pre-loader1");
+
+    window.addEventListener("load", function () {
+      loader.style.display = "none";
+    });
 
 
 
